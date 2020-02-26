6f6feaf26580465d611b108dcf0798b5573f3d32
Alex Miller <alexmillerprojects@gmail.com>
2020-02-25 18:50:36 -0800

    finished project and stretch

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import {Route, Link} from 'react-router-dom';
 
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
@@ -25,24 +26,28 @@ class Login extends React.Component {
         .post('/api/login', this.state.credentials)
         .then(res => {
             console.log(res)
-            // window.localStorage.setItem('token', res.data.payload)1
+            window.localStorage.setItem('token', res.data.payload)
         })
         .catch(err => console.log(err))
+        window.location.replace('http://localhost:3000/api/friends')
     }
 
     render() {
         return (
-            <div>
-                <form>
+            <div className='login-form area'>
+                <h1 className='title'>All your friends, in one place!</h1>
+                <form className='login-form' onSubmit={this.login}>
                     <input
                         type='text'
                         name='username'
+                        placeholder='Username..'
                         value={this.state.credentials.username}
                         onChange={this.handleChange}
                     />
                     <input
                         type='password'
                         name='password'
+                        placeholder='Password..'
                         value={this.state.credentials.password}
                         onChange={this.handleChange}
                     />
